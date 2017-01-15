bd09282a5e189c99ce5d09682f1acc915f67685f
Onur Gunduz <ogunduz@gmail.com>
2017-01-15 17:27:04 +0100

    build meta-model typings using templates

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from "./meta-model";
+export * from "./util";
--- src/meta-model.ts
+++ src/meta-model.ts
@@ -1,8 +1,20 @@
 import {util} from "./util";
+const isInteger = require("is-integer");
+const indentString = require("indent-string");
+
+export function buildMetaModel(model: IMetaModel): MetaModel {
+	return new MetaModel(model);
+}
+
+export function buildSemantics(models: any[]) {
+	return new SemanticsNode(models);
+}
 
 /*
- * The following type definitions _must_ match `./meta-model.json`.
+ * The following type definitions _must_ match with the metamodel json.
  */
+
+export const TYPE_KEY_NAME = "$sType";
 
 export interface IMetaModel {
 	[typeName: string]: IMetaType;
@@ -22,8 +34,6 @@ export interface IPropertyDescription {
 	optional?: boolean;
 	ownType?: boolean;
 }
-
-export const TYPE_KEY_NAME = "$sType";
 
 /**
  * The default instance, exported from this file,
@@ -69,4 +79,159 @@ export function isSemanticsTyped(json: any) {
  */
 export function	sType(object: Object) {
 	return object[TYPE_KEY_NAME];
+}
+
+export class PropertyNode {
+	type: string = "";
+	name: string = "";
+	typeName: string = "";
+	ownType: boolean = false;
+	stringMap: boolean = false;
+	optional: boolean = false;
+
+	template: any = templateFor("Property");
+
+	constructor(opts: any) {
+		this.ownType = !!opts.ownType;
+		this.stringMap = !!opts.stringMap;
+		this.optional = !!opts.optional;
+		this.type = opts.type;
+		this.name = opts.name;
+		this.typeName = !this.ownType ? opts.type : template.capitalize(opts.type);
+	}
+
+	getTemplateArguments() {
+		return [
+			this.name + (this.optional ? "?" : ""),
+			this.stringMap
+				? `{ [name: string]: ${this.typeName} }`
+				: this.ownType
+					? `I${this.typeName}`
+					: this.typeName
+		];
+	}
+
+	print() {
+		return this.template(...this.getTemplateArguments());
+	}
+}
+
+export class InterfaceNode {
+	name: string = "";
+	type: string = "";
+	properties: any[] = [];
+	description: any[] = [];
+
+	template: any = templateFor("Interface");
+
+	constructor(opts: any) {
+		this.name = template.capitalize(opts.type);
+		this.type = opts.type;
+		this.properties = opts.properties;
+		this.description = opts.description;
+	}
+
+	getTemplateArguments() {
+		return {
+			name: this.name,
+			type: this.type,
+			properties: Object.keys(this.properties).map(key => {
+				const p = this.properties[key];
+				return template.indent((new PropertyNode({ name: key, ...p})).print());
+			}).join("\n")
+		};
+	}
+
+	print() {
+		return this.template(this.getTemplateArguments());
+	}
+}
+
+export class SemanticsNode {
+	model: any;
+
+	template: any = templateFor("Semantics");
+
+	constructor(model: any) {
+		this.model = model;
+	}
+
+	getTemplateArguments() {
+		return [
+			Object.keys(this.model).map(key => {
+				const model = { type: key, ...this.model[key] };
+				return new InterfaceNode(model).print();
+			}).join("\n"),
+			Object.keys(this.model).map(key => {
+				return "I" + template.capitalize(key);
+			}).join(" | ")
+		];
+	}
+
+	print() {
+		return this.template(...this.getTemplateArguments()) + "\n";
+	}
+}
+
+export namespace template {
+
+	export function tag(strings: any, ...keys: any[]) {
+		return (function(...values: any[]) {
+			const dict = values[values.length - 1] || {};
+			const result = [strings[0]];
+
+			keys.forEach((key, i) => {
+				const value = isInteger(key) ? values[key] : dict[key];
+				result.push(value, strings[i + 1]);
+			});
+
+			return result.join("");
+		});
+	}
+
+	export function indent(s: string, n = 2, p = " ") {
+		return indentString(String(s), n, p);
+	}
+
+	export function capitalize(s: string) {
+		return s
+			.replace(/-/g, " ")
+			.replace(/\w\S*/g, res => {
+				return res.charAt(0).toUpperCase() + res.substr(1).toLowerCase();
+			})
+			.replace(/[\s\xa0]+/g, "");
+	}
+
+} /* template */
+
+const tag = template.tag;
+
+const templates = {
+
+Property: tag `${0}: ${1}`,
+
+Semantics: tag
+`export interface ISemanticsTyped {
+  $sType: string
+}
+
+${0}
+
+export type SemanticsTyped =
+  ${1}
+`,
+
+Interface: tag
+`export interface I${"name"} extends ISemanticsTyped {
+  $sType: "${"type"}"
+${"properties"}
+}
+`
+
+};
+
+export function templateFor($type: string) {
+	if (templates.hasOwnProperty($type)) {
+		return templates[$type];
+	}
 }
